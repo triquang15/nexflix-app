e756e8afa3c409b1cde08eee578919c307477c52
qnguyen95 <quangtri.nguyen@dxc.com>
2024-01-05 16:09:46 +0700

    @Code Rediect Profile Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import userCurrentUser from '@/hooks/userCurrentUser';
+import { NextPageContext } from 'next'
+import { getSession, signOut } from 'next-auth/react'
 
-const inter = Inter({ subsets: ['latin'] })
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if(!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
+  const { data: user } = userCurrentUser();
   return (
    <>
       <h1>Hello</h1>
+      <p className='text-white'>Logged in as: {user?.name}</p>
+      <button className='h-10 w-full bg-white' onClick={() => signOut()}>Logout</button>
    </>
   )
 }
