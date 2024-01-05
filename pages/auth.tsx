09853a5e6a546b4e5c17127b698f1b9f0469f677
qnguyen95 <quangtri.nguyen@dxc.com>
2024-01-05 12:29:18 +0700

    #Code Login Google & Github

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -3,6 +3,9 @@ import { useCallback, useState } from "react";
 import axios from "axios";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import {FcGoogle} from 'react-icons/fc';
+import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
   const router = useRouter();
@@ -55,7 +58,7 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === 'login' ? 'Sign in' : 'Register'}
+              {variant === 'login' ? 'Sign In' : 'Get Started'}
             </h2>
             <div className="flex flex-col gap-4">
               {variant === 'register' && (
@@ -84,22 +87,24 @@ const Auth = () => {
             </div>
             <button onClick={variant === 'login' ? login : register}
               className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              {variant === 'login' ? 'Login' : 'Sign up'}
+              {variant === 'login' ? 'Sign In' : 'Sign Up'}
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-              {/* <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+              <div onClick={() => signIn('google', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                     <FcGoogle size={32} />
                   </div>
-                  <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                  <div onClick={() => signIn('facebook', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                    <FaFacebook size={32} color="blue" />
+                  </div> 
+                  <div onClick={() => signIn('github', { callbackUrl: '/' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                     <FaGithub size={32} />
-                  </div> */}
+                  </div> 
             </div>
             <p className="text-neutral-500 mt-12">
-              {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+              {variant === 'login' ? 'New to Netflix?' : 'Already have an account?'}
               <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                {variant === 'login' ? 'Create an account' : 'Login'}
+                {variant === 'login' ? 'Sign up now.' : 'Login'}
               </span>
-              .
             </p>
           </div>
         </div>
